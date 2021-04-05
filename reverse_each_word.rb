56674b47bcd07406d6b624b6ad360468134df650
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-04-05 21:38:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,12 @@ def reverse_each_word(array)
   reversed_array = new_array.each {|x| x.reverse!}
   return reversed_array.join(" ")
 end
+
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  new_array.collect {|x| x.reverse!}
+  new_array.join(" ")
+
+  def reverse_each_word(array)
+  array.split(" ").collect {|x| x.reverse!}.join(" ")
+end
