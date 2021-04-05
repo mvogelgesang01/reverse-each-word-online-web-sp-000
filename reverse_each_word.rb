5419a2d9c197b1db7358d480c498229fd41f9dcd
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-04-05 21:38:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,6 +8,7 @@ def reverse_each_word(array)
   new_array = array.split(" ")
   new_array.collect {|x| x.reverse!}
   new_array.join(" ")
+end
 
   def reverse_each_word(array)
   array.split(" ").collect {|x| x.reverse!}.join(" ")
